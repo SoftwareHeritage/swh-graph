bcdce568a02c19bb012e52116de7f63aab2916d9
Thibault Allançon <haltode@gmail.com>
2019-05-06 08:52:54 +0200

    api: client: add cli entry point

---
+++ api/client/swh/graph/cli.py
@@ -0,0 +1,28 @@
+import click
+
+import client
+
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('--host', default='0.0.0.0', help="Host where runs the server")
+@click.option('--port', default='5010', help="Binding port of the server")
+@click.pass_context
+def cli(ctx, host, port):
+    """Software Heritage Graph API
+
+    """
+    url = 'http://' + host + ':' + port
+    app = client.RemoteGraphClient(url)
+
+    print(app.get_nb_nodes())
+
+
+def main():
+    return cli(auto_envvar_prefix='SWH_GRAPH')
+
+
+if __name__ == '__main__':
+    main()
