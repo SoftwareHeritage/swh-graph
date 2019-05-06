a47d54676557cc544d85381c2f7c3a8cfe261bb6
Thibault Allançon <haltode@gmail.com>
2019-05-06 10:12:00 +0200

    compression: add compress script and Dockerfile

---
+++ compression/compress_graph.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Expected two arguments: <input graph> <output dir>"
+    exit -1
+fi
+
+INPUT_GRAPH=$1
+OUTPUT_DIR=$2
+DATASET=$(basename $INPUT_GRAPH)
+COMPR_GRAPH="$OUTPUT_DIR/$DATASET"
+
+java_cmd () {
+    /usr/bin/time -v java -Xmx1024G -cp /app/'*' $*
+}
+
+llp_ordering () {
+    # Create a symmetrized version of the graph
+    # (output: .{graph,offsets,properties})
+    java_cmd it.unimi.dsi.webgraph.Transform symmetrizeOffline \
+        $COMPR_GRAPH-bv $COMPR_GRAPH-bv-sym
+    java_cmd it.unimi.dsi.webgraph.BVGraph --list $COMPR_GRAPH-bv-sym
+
+    # Find a better permutation through Layered LPA
+    java_cmd it.unimi.dsi.law.graph.LayeredLabelPropagation \
+        $COMPR_GRAPH-bv-sym $COMPR_GRAPH.order
+}
+
+bfs_ordering () {
+    java_cmd it.unimi.dsi.law.graph.BFS $COMPR_GRAPH-bv $COMPR_GRAPH.order
+}
+
+mkdir -p $OUTPUT_DIR
+
+# Build a function (MPH) that maps node names to node numbers in lexicographic
+# order (output: .mph)
+java_cmd it.unimi.dsi.sux4j.mph.GOVMinimalPerfectHashFunction \
+    --zipped $COMPR_GRAPH.mph $INPUT_GRAPH.nodes.csv.gz
+
+# Build the graph in BVGraph format (output: .{graph,offsets,properties})
+java_cmd it.unimi.dsi.webgraph.ScatteredArcsASCIIGraph  \
+    --function $COMPR_GRAPH.mph                         \
+    --zipped $COMPR_GRAPH-bv < $INPUT_GRAPH.edges.csv.gz
+# Build the offset big-list file to load the graph faster (output: .obl)
+java_cmd it.unimi.dsi.webgraph.BVGraph --list $COMPR_GRAPH-bv
+
+# Find a better permutation
+bfs_ordering
+
+# Permute the graph accordingly
+java_cmd it.unimi.dsi.webgraph.Transform mapOffline \
+    $COMPR_GRAPH-bv $COMPR_GRAPH $COMPR_GRAPH.order
+java_cmd it.unimi.dsi.webgraph.BVGraph --list $COMPR_GRAPH
+
+# Compute graph statistics (output: .{indegree,outdegree,stats})
+java_cmd it.unimi.dsi.webgraph.Stats $COMPR_GRAPH
