6606fdf1fbf50177c1b15e47f33801db2db853d1
Thibault Allançon <haltode@gmail.com>
2019-05-14 13:51:10 +0900

    api: server: test: add base testing class

---
+++ api/server/src/test/java/org/softwareheritage/graph/GraphTest.java
@@ -0,0 +1,22 @@
+package org.softwareheritage.graph;
+
+import org.junit.BeforeClass;
+
+import org.softwareheritage.graph.Graph;
+
+public class GraphTest
+{
+    static Graph graph;
+
+    @BeforeClass
+    public static void setUp()
+    {
+        String graphPath = System.getProperty("graphPath");
+        graph = new Graph(graphPath);
+    }
+
+    public Graph getGraph()
+    {
+        return graph;
+    }
+}
