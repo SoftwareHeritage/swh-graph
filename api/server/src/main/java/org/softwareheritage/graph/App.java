a06f5b534e66668731eece4bf0d92fbb4c5a0688
Thibault Allançon <haltode@gmail.com>
2019-05-28 15:35:59 +0900

    api: server: add 'visit/' REST API endpoint

--- api/server/src/main/java/org/softwareheritage/graph/App.java
+++ api/server/src/main/java/org/softwareheritage/graph/App.java
@@ -1,12 +1,13 @@
 package org.softwareheritage.graph;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Optional;
 
 import io.javalin.Javalin;
 
 import org.softwareheritage.graph.Graph;
 import org.softwareheritage.graph.algo.Stats;
+import org.softwareheritage.graph.algo.Visit;
 
 public class App {
   public static void main(String[] args) throws IOException, Exception {
@@ -25,6 +26,18 @@ public class App {
         ctx.status(400);
         ctx.result(e.toString());
       }
+    });
+
+    app.get("/visit/:swh_id", ctx -> {
+      String start = ctx.pathParam("swh_id");
+
+      // By default, traversal is a forward DFS using all edges
+      String algorithm = Optional.ofNullable(ctx.queryParam("traversal")).orElse("dfs");
+      String direction = Optional.ofNullable(ctx.queryParam("direction")).orElse("forward");
+      String edges = Optional.ofNullable(ctx.queryParam("edges")).orElse("cnt:dir:rel:rev:snp");
+
+      // TODO: Use transposed graph depending on 'direction'
+      ctx.json(new Visit(graph, start, edges, algorithm));
     });
 
     app.error(404, ctx -> { ctx.result("Not found"); });
