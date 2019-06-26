5158e27d0e38dfb6ee5ac3e0cf99d134cdc0d824
Thibault Allançon <haltode@gmail.com>
2019-06-26 21:12:57 +0900

    dockerfiles: scripts: exit on failure

--- dockerfiles/scripts/compress_graph.sh
+++ dockerfiles/scripts/compress_graph.sh
@@ -52,36 +52,41 @@ java_cmd () {
     # lexicographic order (output: .mph)
     java_cmd it.unimi.dsi.sux4j.mph.GOVMinimalPerfectHashFunction       \
         --zipped $compr_graph_path.mph --temp-dir $tmp_dir              \
-        $graph_path.nodes.csv.gz                                        ;
+        $graph_path.nodes.csv.gz                                        &&
 
     # Build the graph in BVGraph format (output: .{graph,offsets,properties})
     java_cmd it.unimi.dsi.big.webgraph.ScatteredArcsASCIIGraph          \
         --function $compr_graph_path.mph --temp-dir $tmp_dir            \
-        --zipped $compr_graph_path-bv < $graph_path.edges.csv.gz        ;
+        --zipped $compr_graph_path-bv < $graph_path.edges.csv.gz        &&
     # Build the offset big-list file to load the graph faster (output: .obl)
     java_cmd it.unimi.dsi.big.webgraph.BVGraph                          \
-        --list $compr_graph_path-bv                                     ;
+        --list $compr_graph_path-bv                                     &&
 
     # Find a better permutation using a BFS traversal order (output: .order)
     java_cmd it.unimi.dsi.law.graph.BFSBig                              \
-        $compr_graph_path-bv $compr_graph_path.order                    ;
+        $compr_graph_path-bv $compr_graph_path.order                    &&
 
     # Permute the graph accordingly
-    batch_size=1000000000
+    batch_size=1000000000                                               &&
     java_cmd it.unimi.dsi.big.webgraph.Transform mapOffline             \
         $compr_graph_path-bv $compr_graph_path                          \
-        $compr_graph_path.order $batch_size                             ;
+        $compr_graph_path.order $batch_size                             &&
     java_cmd it.unimi.dsi.big.webgraph.BVGraph                          \
-        --list $compr_graph_path                                        ;
+        --list $compr_graph_path                                        &&
 
     # Compute graph statistics (output: .{indegree,outdegree,stats})
-    java_cmd it.unimi.dsi.big.webgraph.Stats $compr_graph_path          ;
+    java_cmd it.unimi.dsi.big.webgraph.Stats $compr_graph_path          &&
 
     # Create transposed graph (to allow backward traversal)
     java_cmd it.unimi.dsi.big.webgraph.Transform transposeOffline       \
-        $compr_graph_path $compr_graph_path-transposed $batch_size      ;
+        $compr_graph_path $compr_graph_path-transposed $batch_size      &&
     java_cmd it.unimi.dsi.big.webgraph.BVGraph                          \
-        --list $compr_graph_path-transposed                             ;
+        --list $compr_graph_path-transposed
 } >> $stdout_file 2>> $stderr_file
 
-echo "Graph compression done."
+if [[ $? -eq 0 ]]; then
+    echo "Graph compression done."
+else
+    echo "Graph compression failed: see $stderr_file for more info."
+    exit 1
+fi
