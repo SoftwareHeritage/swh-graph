5bcfcca2c485cb1974ee95984e7bbeab597655aa
Thibault Allançon <haltode@gmail.com>
2019-07-02 17:05:45 +0200

    server: test: add nodes checks in VisitTest

--- api/server/src/test/java/org/softwareheritage/graph/VisitTest.java
+++ api/server/src/test/java/org/softwareheritage/graph/VisitTest.java
@@ -1,6 +1,7 @@
 package org.softwareheritage.graph;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,18 +14,33 @@ import org.softwareheritage.graph.SwhPath;
 import org.softwareheritage.graph.algo.Visit;
 
 public class VisitTest extends GraphTest {
-  void assertEqualSwhPaths(ArrayList<SwhPath> expecteds, ArrayList<SwhPath> actuals) {
+  Visit.OutputFmt outputFmt = Visit.OutputFmt.NODES_AND_PATHS;
+
+  private void assertEqualPaths(ArrayList<SwhPath> expecteds, ArrayList<SwhPath> actuals) {
     Assert.assertThat(expecteds, containsInAnyOrder(actuals.toArray()));
   }
 
-  Visit.OutputFmt outputFmt = Visit.OutputFmt.NODES_AND_PATHS;
+  private void assertEqualNodes(LinkedHashSet<SwhId> expecteds, LinkedHashSet<SwhId> actuals) {
+    Assert.assertThat(expecteds, containsInAnyOrder(actuals.toArray()));
+  }
+
+  private void assertSameNodesFromPaths(ArrayList<SwhPath> paths, LinkedHashSet<SwhId> nodes) {
+    LinkedHashSet<SwhId> expectedNodes = new LinkedHashSet<SwhId>();
+    for (SwhPath path : paths) {
+      for (SwhId node : path.getPath()) {
+        expectedNodes.add(node);
+      }
+    }
+    assertEqualNodes(expectedNodes, nodes);
+  }
 
   @Test
-  public void dfsForwardFromRoot() {
+  public void forwardFromRoot() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:snp:0000000000000000000000000000000000000020");
     Visit visit = new Visit(graph, swhId, "*", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -109,15 +125,17 @@ public class VisitTest extends GraphTest {
           "swh:1:cnt:0000000000000000000000000000000000000001"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsForwardFromMiddle() {
+  public void forwardFromMiddle() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:dir:0000000000000000000000000000000000000012");
     Visit visit = new Visit(graph, swhId, "*", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -152,15 +170,17 @@ public class VisitTest extends GraphTest {
           "swh:1:cnt:0000000000000000000000000000000000000011"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsForwardFromLeaf() {
+  public void forwardFromLeaf() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:cnt:0000000000000000000000000000000000000004");
     Visit visit = new Visit(graph, swhId, "*", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -168,15 +188,17 @@ public class VisitTest extends GraphTest {
           "swh:1:cnt:0000000000000000000000000000000000000004"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsBackwardFromRoot() {
+  public void backwardFromRoot() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:snp:0000000000000000000000000000000000000020");
     Visit visit = new Visit(graph, swhId, "*", "backward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -184,15 +206,17 @@ public class VisitTest extends GraphTest {
           "swh:1:snp:0000000000000000000000000000000000000020"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsBackwardFromMiddle() {
+  public void backwardFromMiddle() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:dir:0000000000000000000000000000000000000012");
     Visit visit = new Visit(graph, swhId, "*", "backward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -203,15 +227,17 @@ public class VisitTest extends GraphTest {
           "swh:1:rel:0000000000000000000000000000000000000019"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsBackwardFromLeaf() {
+  public void backwardFromLeaf() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:cnt:0000000000000000000000000000000000000004");
     Visit visit = new Visit(graph, swhId, "*", "backward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -252,15 +278,17 @@ public class VisitTest extends GraphTest {
           "swh:1:snp:0000000000000000000000000000000000000020"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsForwardSnpToRev() {
+  public void forwardSnpToRev() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:snp:0000000000000000000000000000000000000020");
     Visit visit = new Visit(graph, swhId, "snp:rev", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -269,15 +297,17 @@ public class VisitTest extends GraphTest {
           "swh:1:rev:0000000000000000000000000000000000000009"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsForwardRelToRevRevToRev() {
+  public void forwardRelToRevRevToRev() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:rel:0000000000000000000000000000000000000010");
     Visit visit = new Visit(graph, swhId, "rel:rev,rev:rev", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -287,15 +317,17 @@ public class VisitTest extends GraphTest {
           "swh:1:rev:0000000000000000000000000000000000000003"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsForwardRevToAllDirToAll() {
+  public void forwardRevToAllDirToAll() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:rev:0000000000000000000000000000000000000013");
     Visit visit = new Visit(graph, swhId, "rev:*,dir:*", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -373,15 +405,17 @@ public class VisitTest extends GraphTest {
           "swh:1:cnt:0000000000000000000000000000000000000001"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsForwardSnpToAllRevToAll() {
+  public void forwardSnpToAllRevToAll() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:snp:0000000000000000000000000000000000000020");
     Visit visit = new Visit(graph, swhId, "snp:*,rev:*", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -403,15 +437,17 @@ public class VisitTest extends GraphTest {
           "swh:1:rel:0000000000000000000000000000000000000010"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsForwardNoEdges() {
+  public void forwardNoEdges() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:snp:0000000000000000000000000000000000000020");
     Visit visit = new Visit(graph, swhId, "", "forward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -419,15 +455,17 @@ public class VisitTest extends GraphTest {
           "swh:1:snp:0000000000000000000000000000000000000020"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
   }
 
   @Test
-  public void dfsBackwardRevToRevRevToRel() {
+  public void backwardRevToRevRevToRel() {
     Graph graph = getGraph();
     SwhId swhId = new SwhId("swh:1:rev:0000000000000000000000000000000000000003");
     Visit visit = new Visit(graph, swhId, "rev:rev,rev:rel", "backward", outputFmt);
     ArrayList<SwhPath> paths = visit.getPaths();
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
 
     ArrayList<SwhPath> expectedPaths = new ArrayList<SwhPath>();
     expectedPaths.add(
@@ -445,6 +483,49 @@ public class VisitTest extends GraphTest {
           "swh:1:rel:0000000000000000000000000000000000000010"
         ));
 
-    assertEqualSwhPaths(expectedPaths, paths);
+    assertEqualPaths(expectedPaths, paths);
+    assertSameNodesFromPaths(expectedPaths, nodes);
+  }
+
+  @Test
+  public void forwardFromRootNodesOnly() {
+    Graph graph = getGraph();
+    SwhId swhId = new SwhId("swh:1:snp:0000000000000000000000000000000000000020");
+    Visit visit = new Visit(graph, swhId, "*", "forward", Visit.OutputFmt.ONLY_NODES);
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
+
+    LinkedHashSet<SwhId> expectedNodes = new LinkedHashSet<SwhId>();
+    expectedNodes.add(new SwhId("swh:1:snp:0000000000000000000000000000000000000020"));
+    expectedNodes.add(new SwhId("swh:1:rel:0000000000000000000000000000000000000010"));
+    expectedNodes.add(new SwhId("swh:1:rev:0000000000000000000000000000000000000009"));
+    expectedNodes.add(new SwhId("swh:1:rev:0000000000000000000000000000000000000003"));
+    expectedNodes.add(new SwhId("swh:1:dir:0000000000000000000000000000000000000002"));
+    expectedNodes.add(new SwhId("swh:1:cnt:0000000000000000000000000000000000000001"));
+    expectedNodes.add(new SwhId("swh:1:dir:0000000000000000000000000000000000000008"));
+    expectedNodes.add(new SwhId("swh:1:dir:0000000000000000000000000000000000000006"));
+    expectedNodes.add(new SwhId("swh:1:cnt:0000000000000000000000000000000000000004"));
+    expectedNodes.add(new SwhId("swh:1:cnt:0000000000000000000000000000000000000005"));
+    expectedNodes.add(new SwhId("swh:1:cnt:0000000000000000000000000000000000000007"));
+
+    assertEqualNodes(expectedNodes, nodes);
+  }
+
+  @Test
+  public void backwardRevToAllNodesOnly() {
+    Graph graph = getGraph();
+    SwhId swhId = new SwhId("swh:1:rev:0000000000000000000000000000000000000003");
+    Visit visit = new Visit(graph, swhId, "rev:*", "backward", Visit.OutputFmt.ONLY_NODES);
+    LinkedHashSet<SwhId> nodes = visit.getNodes();
+
+    LinkedHashSet<SwhId> expectedNodes = new LinkedHashSet<SwhId>();
+    expectedNodes.add(new SwhId("swh:1:rev:0000000000000000000000000000000000000003"));
+    expectedNodes.add(new SwhId("swh:1:rev:0000000000000000000000000000000000000009"));
+    expectedNodes.add(new SwhId("swh:1:snp:0000000000000000000000000000000000000020"));
+    expectedNodes.add(new SwhId("swh:1:rel:0000000000000000000000000000000000000010"));
+    expectedNodes.add(new SwhId("swh:1:rev:0000000000000000000000000000000000000013"));
+    expectedNodes.add(new SwhId("swh:1:rev:0000000000000000000000000000000000000018"));
+    expectedNodes.add(new SwhId("swh:1:rel:0000000000000000000000000000000000000019"));
+
+    assertEqualNodes(expectedNodes, nodes);
   }
 }
