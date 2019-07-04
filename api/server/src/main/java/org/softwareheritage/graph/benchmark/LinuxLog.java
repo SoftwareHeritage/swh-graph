8e99fa365bda1756f773b5f074badc2ebf6fd2ac
Thibault Allançon <haltode@gmail.com>
2019-07-04 16:15:27 +0200

    server: benchmark: use new API in LinuxLog

--- api/server/src/main/java/org/softwareheritage/graph/benchmark/LinuxLog.java
+++ api/server/src/main/java/org/softwareheritage/graph/benchmark/LinuxLog.java
@@ -1,10 +1,6 @@
 package org.softwareheritage.graph.benchmark;
 
 import java.io.IOException;
-import java.util.Stack;
-
-import it.unimi.dsi.big.webgraph.LazyLongIterator;
-import it.unimi.dsi.bits.LongArrayBitVector;
 
 import org.softwareheritage.graph.Graph;
 import org.softwareheritage.graph.SwhId;
@@ -15,48 +11,21 @@ public class LinuxLog {
     String path = args[0];
     Graph graph = new Graph(path);
 
+    // A linux kernel commit on Sun Dec 31
     final SwhId commit = new SwhId("swh:1:rev:f39d7d78b70e0f39facb1e4fab77ad3df5c52a35");
     final long expectedCount = 723640;
     System.out.println("git log " + commit);
-
-    long count = countCommits(graph, commit);
-    System.out.println("Counted " + count + " commits");
-    if (count != expectedCount) {
-      throw new IllegalArgumentException("Expected " + expectedCount + " commits");
-    }
+    System.out.println("Expecting " + expectedCount + " commits");
 
     long startTime = System.nanoTime();
-    Visit visit = new Visit(graph, commit, "rev", "dfs", "backward");
+    Visit visit = new Visit(graph, commit, "rev:rev", "forward", Visit.OutputFmt.ONLY_NODES);
     long endTime = System.nanoTime();
     double duration = (double) (endTime - startTime) / 1_000_000_000;
     System.out.println("Visit operation done in: " + duration + " seconds");
-  }
 
-  static long countCommits(Graph graph, SwhId commit) {
-    final boolean useTransposed = true;
-
-    Stack<Long> stack = new Stack<Long>();
-    LongArrayBitVector visited = LongArrayBitVector.ofLength(graph.getNbNodes());
-    long startNodeId = graph.getNodeId(commit);
-    long count = 0;
-
-    stack.push(startNodeId);
-    visited.set(startNodeId);
-    while (!stack.empty()) {
-      long currentNodeId = stack.pop();
-      count++;
-
-      long degree = graph.degree(currentNodeId, useTransposed);
-      LazyLongIterator revisions = graph.neighbors(currentNodeId, useTransposed);
-      while (degree-- > 0) {
-        long nextNodeId = revisions.nextLong();
-        if (!visited.getBoolean(nextNodeId)) {
-          visited.set(nextNodeId);
-          stack.push(nextNodeId);
-        }
-      }
+    long count = visit.getNodes().size();
+    if (count != expectedCount) {
+      throw new IllegalArgumentException("Counted " + count + " commits");
     }
-
-    return count;
   }
 }
