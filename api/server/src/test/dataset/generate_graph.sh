6e9bb13c272c33d66ef8db86680584b0301498c1
Thibault Allançon <haltode@gmail.com>
2019-07-09 22:34:35 +0200

    server: test: rework automatic dataset generation
    - Rename 'graph' to more explicit 'example'
    - Clean previous outputs before each run
    - Use two distinct volumes for input/output

--- api/server/src/test/dataset/generate_graph.sh
+++ api/server/src/test/dataset/generate_graph.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Clean previous run
+rm -rf dockerfiles output
+mkdir output
+
 # Build Docker work environment
 toplevel_dir=`git rev-parse --show-toplevel`
 mkdir -p dockerfiles
@@ -7,16 +11,17 @@ cp -r $toplevel_dir/dockerfiles/ .
 docker build --tag swh-graph-test dockerfiles
 
 # Setup input for compression script
-tr ' ' '\n' < graph.edges.csv | sort -u > graph.nodes.csv
-gzip --force --keep graph.edges.csv
-gzip --force --keep graph.nodes.csv
-
-# Setup output
-rm -f stderr stdout
+tr ' ' '\n' < example.edges.csv | sort -u > example.nodes.csv
+gzip --force --keep example.edges.csv
+gzip --force --keep example.nodes.csv
 
 docker run                                                      \
+    --user $(id -u):$(id -g)                                    \
     --name swh-graph-test --rm --tty --interactive              \
-    --volume $(pwd):/data swh-graph-test:latest                 \
+    --volume $(pwd):/input                                      \
+    --volume $(pwd)/output:/output                              \
+    swh-graph-test:latest                                       \
     ./scripts/compress_graph.sh                                 \
-    --input /data/graph --output /data --lib /swh/graph-lib     \
-    --stdout /data/stdout --stderr /data/stderr
+    --input /input/example --output /output                     \
+    --lib /swh/graph-lib --tmp /output/tmp                      \
+    --stdout /output/stdout --stderr /output/stderr
