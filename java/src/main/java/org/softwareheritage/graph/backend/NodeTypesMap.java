fcf6f1bf07763b318d38b9243ff9dfa35d6470c2
Stefano Zacchiroli <zack@upsilon.cc>
2019-11-09 10:45:59 +0100

    javadoc: fix docstring syntax error in NodeTypesMap
    fix docs.s.o build failure

--- java/src/main/java/org/softwareheritage/graph/backend/NodeTypesMap.java
+++ java/src/main/java/org/softwareheritage/graph/backend/NodeTypesMap.java
@@ -13,9 +13,9 @@ import org.softwareheritage.graph.Node;
  * href="https://docs.softwareheritage.org/devel/swh-model/data-model.html">data
  * model</a>.
  *
- * The type mapping is pre-computed and dumped on disk in the {@link *
- * MapBuilder} class, then it is loaded in-memory here using <a
- * href="http://fastutil.di.unimi.it/">fastutil</a> LongBigList. To be
+ * The type mapping is pre-computed and dumped on disk in the {@link MapBuilder}
+ * class, then it is loaded in-memory here using
+ * <a href="http://fastutil.di.unimi.it/">fastutil</a> LongBigList. To be
  * space-efficient, the mapping is stored as a bitmap using minimum number of
  * bits per {@link Node.Type}.
  *
