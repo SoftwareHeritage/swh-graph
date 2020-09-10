4e2a94b3f3b29cde8b1e51d75b64bcdec8db0782
Antoine Pietri <antoine.pietri1@gmail.com>
2020-09-10 09:43:36 +0200

    java: cleanup MPHTranslate

--- java/src/main/java/org/softwareheritage/graph/utils/MPHTranslate.java
+++ java/src/main/java/org/softwareheritage/graph/utils/MPHTranslate.java
@@ -5,20 +5,17 @@ import it.unimi.dsi.fastutil.io.BinIO;
 import it.unimi.dsi.fastutil.objects.Object2LongFunction;
 import it.unimi.dsi.io.FastBufferedReader;
 import it.unimi.dsi.io.LineIterator;
-import org.softwareheritage.graph.experiments.multiplicationfactor.GenDistribution;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 public class MPHTranslate {
-    final static String SORT_BUFFER_SIZE = "40%";
-
     private static JSAPResult parse_args(String[] args) {
         JSAPResult config = null;
         try {
             SimpleJSAP jsap = new SimpleJSAP(
-                    GenDistribution.class.getName(),
+                    MPHTranslate.class.getName(),
                     "",
                     new Parameter[]{
                             new UnflaggedOption("function", JSAP.STRING_PARSER, JSAP.REQUIRED,
@@ -36,25 +33,21 @@ public class MPHTranslate {
         return config;
     }
 
-    public static void main(String[] args) throws IOException {
-        JSAPResult config = parse_args(args);
-        String mphPath = config.getString("function");
-        outputPermutation(mphPath);
+    @SuppressWarnings("unchecked") // Suppress warning for Object2LongFunction cast
+    static Object2LongFunction<String> loadMPH(String mphPath)
+            throws IOException, ClassNotFoundException {
+        return (Object2LongFunction<String>) BinIO.loadObject(mphPath);
     }
 
+    public static void main(String[] args)
+            throws IOException, ClassNotFoundException {
+        JSAPResult config = parse_args(args);
+        String mphPath = config.getString("function");
 
-    @SuppressWarnings("unchecked") // Suppress warning for Object2LongFunction cast
-    static void outputPermutation(String mphPath)
-            throws IOException {
-        Object2LongFunction<String> mphMap = null;
-        try {
-            mphMap = (Object2LongFunction<String>) BinIO.loadObject(mphPath);
-        } catch (ClassNotFoundException e) {
-            System.exit(2);
-        }
+        Object2LongFunction<String> mphMap = loadMPH(mphPath);
 
-        FastBufferedReader buffer = new FastBufferedReader(new InputStreamReader(System.in,
-                StandardCharsets.US_ASCII));
+        FastBufferedReader buffer = new FastBufferedReader(
+                new InputStreamReader(System.in, StandardCharsets.US_ASCII));
         LineIterator lineIterator = new LineIterator(buffer);
 
         while (lineIterator.hasNext()) {
