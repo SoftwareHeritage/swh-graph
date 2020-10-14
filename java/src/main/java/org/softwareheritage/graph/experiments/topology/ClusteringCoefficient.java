34ba6645574dc496c3e72b5d146bebb46427f45e
Antoine Pietri <antoine.pietri1@gmail.com>
2020-10-14 17:31:13 +0200

    topology: ClusteringCoefficient: version with subgraph

--- java/src/main/java/org/softwareheritage/graph/experiments/topology/ClusteringCoefficient.java
+++ java/src/main/java/org/softwareheritage/graph/experiments/topology/ClusteringCoefficient.java
@@ -1,38 +1,59 @@
 package org.softwareheritage.graph.experiments.topology;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import com.martiansoftware.jsap.*;
+import it.unimi.dsi.Util;
 import it.unimi.dsi.big.webgraph.ImmutableGraph;
 import it.unimi.dsi.big.webgraph.LazyLongIterator;
+import it.unimi.dsi.fastutil.BigArrays;
+import it.unimi.dsi.fastutil.longs.LongBigArrays;
 import it.unimi.dsi.logging.ProgressLogger;
-import org.slf4j.LoggerFactory;
+import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
+import org.softwareheritage.graph.AllowedNodes;
 import org.softwareheritage.graph.Graph;
+import org.softwareheritage.graph.Node;
+import org.softwareheritage.graph.Subgraph;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.*;
 
 public class ClusteringCoefficient {
-    private Graph graph;
+    private final Graph graph;
+    private final Subgraph subgraph;
+    private final String outdirPath;
+    private final ConcurrentHashMap<Long, Long> result_full;
+    private final ConcurrentHashMap<Long, Long> result_dircnt;
+    private final ConcurrentHashMap<Long, Long> result_rev;
+    private final ConcurrentHashMap<Long, Long> result_revrel;
+    private final ConcurrentHashMap<Long, Long> result_orisnp;
 
-    private void load_graph(String graphBasename) throws IOException {
+    public ClusteringCoefficient(String graphBasename, String allowedNodes, String outdirPath) throws IOException {
+        this.outdirPath = outdirPath;
         System.err.println("Loading graph " + graphBasename + " ...");
-        this.graph = new Graph(graphBasename).symmetrize();
+        this.graph = new Graph(graphBasename);
+        this.subgraph = new Subgraph(this.graph, new AllowedNodes(allowedNodes));
         System.err.println("Graph loaded.");
+
+        result_full = new ConcurrentHashMap<>();
+        result_dircnt = new ConcurrentHashMap<>();
+        result_rev = new ConcurrentHashMap<>();
+        result_revrel = new ConcurrentHashMap<>();
+        result_orisnp = new ConcurrentHashMap<>();
     }
 
     private static JSAPResult parse_args(String[] args) {
         JSAPResult config = null;
         try {
-            SimpleJSAP jsap = new SimpleJSAP(ClusteringCoefficient.class.getName(), "",
+            SimpleJSAP jsap = new SimpleJSAP(AveragePaths.class.getName(), "",
                     new Parameter[]{
                             new FlaggedOption("graphPath", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'g',
                                     "graph", "Basename of the compressed graph"),
+                            new FlaggedOption("nodeTypes", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 's',
+                                    "nodetypes", "Node type constraints"),
                             new FlaggedOption("outdir", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'o',
-                                    "outdir", "Directory where to put the results"),});
+                                    "outdir", "Directory where to put the results"),
+                            new FlaggedOption("numThreads", JSAP.INTEGER_PARSER, "32", JSAP.NOT_REQUIRED, 't',
+                                    "numthreads", "Number of threads"),});
 
             config = jsap.parse(args);
             if (jsap.messagePrinted()) {
@@ -44,22 +65,176 @@ public class ClusteringCoefficient {
         return config;
     }
 
-    private long oppositeEdges(ImmutableGraph graph, long node) {
-        System.out.format("%d\n", node);
+    private void run(int numThreads) throws InterruptedException {
+        final long END_OF_QUEUE = -1L;
+
+        ArrayBlockingQueue<Long> queue = new ArrayBlockingQueue<>(numThreads);
+        ExecutorService service = Executors.newFixedThreadPool(numThreads + 1);
+
+        service.submit(() -> {
+            try {
+                Graph thread_graph = graph.copy();
+                Subgraph thread_subgraph = subgraph.copy();
+
+                long[][] randomPerm = Util.identity(thread_graph.numNodes());
+                LongBigArrays.shuffle(randomPerm, new XoRoShiRo128PlusRandom());
+                long n = thread_graph.numNodes();
+
+                ProgressLogger pl = new ProgressLogger();
+                pl.expectedUpdates = n;
+                pl.itemsName = "nodes";
+                pl.start("Filling processor queue...");
+
+                for (long j = 0; j < n; ++j) {
+                    long node = BigArrays.get(randomPerm, j);
+                    if (thread_subgraph.nodeExists(node) && thread_subgraph.outdegree(node) == 0) {
+                        queue.put(node);
+                    }
+                    if (j % 10000 == 0) {
+                        printResult();
+                    }
+                    pl.update();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                for (int i = 0; i < numThreads; ++i) {
+                    try {
+                        queue.put(END_OF_QUEUE);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        for (int i = 0; i < numThreads; ++i) {
+            service.submit(() -> {
+                try {
+                    Subgraph thread_subgraph = subgraph.copy();
+                    while (true) {
+                        Long node = null;
+                        try {
+                            node = queue.take();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        if (node == null || node == END_OF_QUEUE) {
+                            return;
+                        }
+
+                        computeAt(thread_subgraph, node);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        service.shutdown();
+        service.awaitTermination(365, TimeUnit.DAYS);
+    }
+
+    private void computeAt(Subgraph graph, long node) {
+        long d = graph.outdegree(node);
+        if (d < 2) {
+            return;
+        }
+        Node.Type nodeType = graph.getNodeType(node);
+
         HashSet<Long> neighborhood = new HashSet<>();
         long succ;
         final LazyLongIterator iterator = graph.successors(node);
         while ((succ = iterator.nextLong()) != -1) {
-            System.out.format("%d neighbor add %d\n", node, succ);
+            neighborhood.add(succ);
+        }
+
+        long triangles_full = 0;
+        long triangles_dircnt = 0;
+        long triangles_rev = 0;
+        long triangles_revrel = 0;
+        long triangles_orisnp = 0;
+
+        for (Long neighbor : neighborhood) {
+            Node.Type neighborNodeType = graph.getNodeType(neighbor);
+            final LazyLongIterator it = graph.successors(neighbor);
+            while ((succ = it.nextLong()) != -1) {
+                if (neighborhood.contains(succ)) {
+                    Node.Type succNodeType = graph.getNodeType(succ);
+                    triangles_full++;
+                    if ((nodeType == Node.Type.DIR || nodeType == Node.Type.CNT)
+                            && (neighborNodeType == Node.Type.DIR || neighborNodeType == Node.Type.CNT)
+                            && (succNodeType == Node.Type.DIR || succNodeType == Node.Type.CNT)) {
+                        triangles_dircnt++;
+                    } else if ((nodeType == Node.Type.REV || nodeType == Node.Type.REL)
+                            && (neighborNodeType == Node.Type.REV || neighborNodeType == Node.Type.REL)
+                            && (succNodeType == Node.Type.REV || succNodeType == Node.Type.REL)) {
+                        triangles_revrel++;
+                        if (nodeType == Node.Type.REV && neighborNodeType == Node.Type.REV
+                                && succNodeType == Node.Type.REV)
+                            triangles_rev++;
+                    } else if ((nodeType == Node.Type.ORI || nodeType == Node.Type.SNP)
+                            && (neighborNodeType == Node.Type.ORI || neighborNodeType == Node.Type.SNP)
+                            && (succNodeType == Node.Type.ORI || succNodeType == Node.Type.SNP)) {
+                        triangles_orisnp++;
+                    }
+                }
+            }
+        }
+
+        result_full.merge(triangles_full, 1L, Long::sum);
+        result_dircnt.merge(triangles_dircnt, 1L, Long::sum);
+        result_rev.merge(triangles_rev, 1L, Long::sum);
+        result_revrel.merge(triangles_revrel, 1L, Long::sum);
+        result_orisnp.merge(triangles_orisnp, 1L, Long::sum);
+    }
+
+    public void printSortedDistribution(String distribPath, Map<Long, Long> distrib) throws IOException {
+        PrintWriter f = new PrintWriter(new FileWriter(distribPath));
+        TreeMap<Long, Long> sortedDistribution = new TreeMap<>(distrib);
+        for (Map.Entry<Long, Long> entry : sortedDistribution.entrySet()) {
+            f.println(entry.getKey() + " " + entry.getValue());
+        }
+        f.close();
+    }
+
+    public void printResult() throws IOException {
+        new File(outdirPath).mkdirs();
+
+        printSortedDistribution(outdirPath + "/distribution-full.txt", result_full);
+        printSortedDistribution(outdirPath + "/distribution-dircnt.txt", result_dircnt);
+        printSortedDistribution(outdirPath + "/distribution-rev.txt", result_rev);
+        printSortedDistribution(outdirPath + "/distribution-revrel.txt", result_revrel);
+        printSortedDistribution(outdirPath + "/distribution-orisnp.txt", result_orisnp);
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        JSAPResult config = parse_args(args);
+
+        String graphPath = config.getString("graphPath");
+        String outdir = config.getString("outdir");
+        String allowedNodes = config.getString("nodeTypes");
+        int numThreads = config.getInt("numThreads");
+
+        ClusteringCoefficient cc = new ClusteringCoefficient(graphPath, allowedNodes, outdir);
+        cc.run(numThreads);
+        cc.printResult();
+    }
+
+    // Old unused functions
+
+    private long oppositeEdges(ImmutableGraph graph, long node) {
+        HashSet<Long> neighborhood = new HashSet<>();
+        long succ;
+        final LazyLongIterator iterator = graph.successors(node);
+        while ((succ = iterator.nextLong()) != -1) {
             neighborhood.add(succ);
         }
 
         long closed_triplets = 0;
         for (Long neighbor : neighborhood) {
-            System.out.format("%d neighbor visit %d\n", node, neighbor);
             final LazyLongIterator it = graph.successors(neighbor);
             while ((succ = it.nextLong()) != -1) {
-                System.out.format("%d neighbor visit %d succ %d\n", node, neighbor, succ);
                 if (neighborhood.contains(succ)) {
                     closed_triplets++;
                 }
@@ -154,36 +329,6 @@ public class ClusteringCoefficient {
                 out_global.format("C: %f (triangles: %d, trials: %d)\n", gC, triangles, trials);
                 System.out.format("C: %f (triangles: %d, trials: %d)\n", gC, triangles, trials);
             }
-        }
-    }
-
-    public static void main(String[] args) {
-        JSAPResult config = parse_args(args);
-
-        String graphPath = config.getString("graphPath");
-        String outdirPath = config.getString("outdir");
-
-        ClusteringCoefficient ccoef = new ClusteringCoefficient();
-        try {
-            ccoef.load_graph(graphPath);
-        } catch (IOException e) {
-            System.out.println("Could not load graph: " + e);
-            System.exit(2);
-        }
-
-        Logger rootLogger = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
-        rootLogger.setLevel(Level.DEBUG);
-
-        new File(outdirPath).mkdirs();
-
-        try {
-            ccoef.compute_approx(new Formatter(outdirPath + "/local.txt"));
-            /*
-             * ccoef.compute( symmetric, new ProgressLogger(rootLogger), new Formatter(outdirPath +
-             * "/local.txt"), new Formatter(outdirPath + "/global.txt") );
-             */
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
     }
 }
