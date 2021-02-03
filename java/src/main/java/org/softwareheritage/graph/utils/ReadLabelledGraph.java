5a3d60748fd171ff30b817fac4901892f0ca67b6
Antoine Pietri <antoine.pietri1@gmail.com>
2021-02-03 15:12:29 +0100

    ReadLabelledGraph: use FCL instead of PFCL

--- java/src/main/java/org/softwareheritage/graph/utils/ReadLabelledGraph.java
+++ java/src/main/java/org/softwareheritage/graph/utils/ReadLabelledGraph.java
@@ -1,5 +1,6 @@
 package org.softwareheritage.graph.utils;
 
+import it.unimi.dsi.big.util.FrontCodedStringBigList;
 import it.unimi.dsi.big.util.PermutedFrontCodedStringBigList;
 import it.unimi.dsi.big.webgraph.labelling.ArcLabelledImmutableGraph;
 import it.unimi.dsi.big.webgraph.labelling.ArcLabelledNodeIterator;
@@ -16,7 +17,7 @@ public class ReadLabelledGraph {
 
         ArcLabelledImmutableGraph graph = BitStreamArcLabelledImmutableGraph.loadOffline(graphPath + "-labelled");
         NodeIdMap nodeMap = new NodeIdMap(graphPath, graph.numNodes());
-        PermutedFrontCodedStringBigList filenameMap = (PermutedFrontCodedStringBigList) BinIO
+        FrontCodedStringBigList filenameMap = (FrontCodedStringBigList) BinIO
                 .loadObject(graphPath + "-labels.fcl");
 
         ArcLabelledNodeIterator it = graph.nodeIterator();
