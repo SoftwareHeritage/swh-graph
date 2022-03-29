3563007e9adedc4addb27a2cd9299a94463d7e96
Antoine Pietri <antoine.pietri1@gmail.com>
2022-03-29 16:34:29 +0200

    Ignore typing in generate_dataset.py to avoid hard dependency to swh-dataset

--- swh/graph/tests/dataset/generate_dataset.py
+++ swh/graph/tests/dataset/generate_dataset.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# type: ignore
+
 # Copyright (C) 2021  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
