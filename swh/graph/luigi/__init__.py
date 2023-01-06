d4717386c71f28088a6dd90d4d04ef9024cf618c
Valentin Lorentz <vlorentz@softwareheritage.org>
2023-01-06 11:04:05 +0100

    cli: Add more useful defaults
    two paths + reimport tasks at the package level so they are automatically
    picked up by luigi without passing all module names on the CLI

--- swh/graph/luigi/__init__.py
+++ swh/graph/luigi/__init__.py
@@ -51,7 +51,9 @@ from typing import List
 
 import luigi
 
-from . import compressed_graph
+from .compressed_graph import *  # noqa
+from .misc_datasets import *  # noqa
+from .origin_contributors import *  # noqa
 
 
 class RunExportCompressUpload(luigi.Task):
@@ -64,9 +66,11 @@ class RunExportCompressUpload(luigi.Task):
         """
         from swh.dataset.luigi import RunExportAll
 
+        from .compressed_graph import UploadGraphToS3
+
         return [
             RunExportAll(),
-            compressed_graph.UploadGraphToS3(),
+            UploadGraphToS3(),
         ]
 
     def complete(self) -> bool:
