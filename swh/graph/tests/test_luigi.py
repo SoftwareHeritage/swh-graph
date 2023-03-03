9423751e07b59b9d36e7d8ea27e9202b7d683db1
Valentin Lorentz <vlorentz@softwareheritage.org>
2023-03-03 14:13:55 +0100

    tests: Set a low value for -Xmx
    It defaults to 90% of the total RAM, which means Java effectively never frees
    any memory, and it prevents running more than a couple of tests in parallel.

--- swh/graph/tests/test_luigi.py
+++ swh/graph/tests/test_luigi.py
@@ -30,6 +30,7 @@ def test_compressgraph(tmpdir, workers):
         "testdataset",
         "CompressGraph",
         "--batch-size=1000",  # small value, to go fast on the trivial dataset
+        "--max-ram=70M",
         "--",
         "--local-scheduler",
         "--CompressGraph-local-export-path",
