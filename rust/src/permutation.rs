0a20f5b6b0394c5d91f9897d121d94e3dc9cf885
Valentin Lorentz <vlorentz@softwareheritage.org>
2023-08-23 16:56:14 +0200

    permutation: Check the array contains no duplicates

--- rust/src/permutation.rs
+++ rust/src/permutation.rs
@@ -5,15 +5,24 @@
 
 use std::mem::MaybeUninit;
 use std::path::Path;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use anyhow::{bail, Context, Result};
 use byteorder::{BigEndian, ReadBytesExt};
 use rayon::prelude::*;
 
+/// An array of `n` unique integers in the `0..n` range.
 pub struct Permutation(Vec<usize>);
 
 impl Permutation {
-    pub fn new(num_nodes: usize, path: &Path) -> Result<Permutation> {
+    /// Loads a permutation from disk and returns IO errors if any.
+    ///
+    /// # Safety
+    ///
+    /// This function is not unsafe per-se, but it does not check node ids in the
+    /// permutation's image are correct or unique, which will violate assumptions
+    /// of other unsafe functions down the line.
+    pub unsafe fn new_unchecked(num_nodes: usize, path: &Path) -> Result<Permutation> {
         assert_eq!(
             usize::BITS,
             u64::BITS,
@@ -33,7 +42,17 @@ impl Permutation {
         // read_u64_into() called read_exact(), which checked the length
         unsafe { perm.set_len(num_nodes) };
 
+        Ok(Permutation(perm))
+    }
+
+    /// Loads a permutation from disk, and returns errors in case of IO errors
+    /// or incorrect permutations.
+    pub fn new(num_nodes: usize, path: &Path) -> Result<Permutation> {
+        let perm = unsafe { Permutation::new_unchecked(num_nodes, path) }?;
+
+        // Check the permutation's image has the same size as the preimage
         if let Some((old, new)) = perm
+            .as_ref()
             .par_iter()
             .enumerate()
             .find_any(|&(_old, &new)| new >= num_nodes)
@@ -46,7 +65,27 @@ impl Permutation {
             );
         }
 
-        Ok(Permutation(perm))
+        // Check the permutation is injective
+        let mut seen = Vec::with_capacity(num_nodes as _);
+        seen.extend((0..num_nodes).map(|_| AtomicBool::new(false)));
+        if let Some((old, _)) = perm
+            .as_ref()
+            .par_iter()
+            .map(|&node| seen[node].fetch_or(true, Ordering::SeqCst))
+            .enumerate()
+            .find_any(|&(_node, is_duplicated)| is_duplicated)
+        {
+            let new = perm[old];
+            bail!(
+                "At least two nodes are mapped to {}; one of them is {}",
+                new,
+                old,
+            );
+        }
+
+        // Therefore, the permutation is bijective.
+
+        Ok(perm)
     }
 }
 
