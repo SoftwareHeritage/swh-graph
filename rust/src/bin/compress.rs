SoftwareHeritage/swh-graph

commit 53bc48e2884df8a06c2a8b72d88dd421aa82f927

Update webgraph

rust/src/compress/bv.rs
@@ -41,7 +41,7 @@ pub fn bv<MPHF: SwhidMphf + Sync>(
     let counters = thread_local::ThreadLocal::new();
     let temp_dir = tempfile::tempdir().context("Could not get temporary_directory")?;
     let sorted_arcs = par_sort_arcs(
-        temp_dir.path(),
+        &temp_dir.path().join("sorted_arcs"),
         sort_batch_size,
         iter_arcs(&dataset_dir, allowed_node_types).inspect(|_| {
             // This is safe because only this thread accesses this and only from
@@ -96,12 +96,13 @@ pub fn bv<MPHF: SwhidMphf + Sync>(
     let comp_flags = Default::default();
     let num_threads = num_cpus::get();
 
-    webgraph::graph::bvgraph::parallel_compress_sequential_iter::<lender::Inspect<_, _>>(
+    webgraph::graph::bvgraph::parallel_compress_sequential_iter::<lender::Inspect<_, _>, _>(
         target_dir,
         adjacency_lists,
         num_nodes,
         comp_flags,
         num_threads,
+        temp_dir.path().join("bv"),
     )
     .context("Could not build BVGraph from arcs")?;
 

rust/Cargo.toml
@@ -17,7 +17,7 @@ anyhow = {version="1.0.71", features=["backtrace"]}
 mmap-rs = "0.5.0"
 sux = {git = "https://github.com/vigna/sux-rs", rev = "e081b427f99eeb5ce127dc74c5c03cd0862912c2" }
 #sux = {path = "../../../sux-rs" }
-webgraph = {git = "https://github.com/vigna/webgraph-rs", rev = "e78cf53a33241455efaac903a2113c755cf3f235" }
+webgraph = {git = "https://github.com/vigna/webgraph-rs", rev = "3abb0e33fc7e4b2b8afd0f17ca0a240d42b9437c" }
 #webgraph = { path = "../../../webgraph-rs" }
 log = "0.4.17"
 libc = "0.2.147"

commit f00bb8863178aae58a2d54ed5e43ff887230036b

Add missing tempdir creation, broken by 53bc48e2884df8a06c2a8b72d88dd421aa82f927

rust/src/bin/compress.rs
@@ -815,7 +815,7 @@ pub fn main() -> Result<()> {
                 || allowed_node_types.contains(&SWHType::Release)
             {
                 let Some(person_function) = person_function else {
-                    bail!("--person-mph must be provided unless --allowed-node-types is set to contain neither 'rev' nor 'rel'.");
+                    bail!("--person-function must be provided unless --allowed-node-types is set to contain neither 'rev' nor 'rel'.");
                 };
                 Some(
                     ph::fmph::Function::load(&person_function)

rust/src/compress/bv.rs
@@ -40,8 +40,11 @@ pub fn bv<MPHF: SwhidMphf + Sync>(
     let pl = Mutex::new(pl);
     let counters = thread_local::ThreadLocal::new();
     let temp_dir = tempfile::tempdir().context("Could not get temporary_directory")?;
+    let sorted_arcs_path = temp_dir.path().join("sorted_arcs");
+    std::fs::create_dir(&sorted_arcs_path)
+        .with_context(|| format!("Could not create {}", sorted_arcs_path.display()))?;
     let sorted_arcs = par_sort_arcs(
-        &temp_dir.path().join("sorted_arcs"),
+        &sorted_arcs_path,
         sort_batch_size,
         iter_arcs(&dataset_dir, allowed_node_types).inspect(|_| {
             // This is safe because only this thread accesses this and only from
@@ -96,13 +99,16 @@ pub fn bv<MPHF: SwhidMphf + Sync>(
     let comp_flags = Default::default();
     let num_threads = num_cpus::get();
 
+    let temp_bv_dir = temp_dir.path().join("bv");
+    std::fs::create_dir(&temp_bv_dir)
+        .with_context(|| format!("Could not create {}", temp_bv_dir.display()))?;
     webgraph::graph::bvgraph::parallel_compress_sequential_iter::<lender::Inspect<_, _>, _>(
         target_dir,
         adjacency_lists,
         num_nodes,
         comp_flags,
         num_threads,
-        temp_dir.path().join("bv"),
+        &temp_bv_dir,
     )
     .context("Could not build BVGraph from arcs")?;
 

rust/src/utils/sort.rs
@@ -270,6 +270,10 @@ fn flush(temp_dir: &Path, buffer: &mut [(usize, usize)]) -> Result<BatchIterator
     // but without parallelism, which would cause Rayon to re-enter
     // par_sort_arcs and cause deadlocks: https://github.com/rayon-rs/rayon/issues/1083
     buffer.sort_unstable_by_key(|(src, dst)| (*src, *dst));
-    BatchIterator::new_from_vec_sorted(&sorter_temp_file, buffer)
-        .context("Could not create BatchIterator")
+    BatchIterator::new_from_vec_sorted(&sorter_temp_file, buffer).with_context(|| {
+        format!(
+            "Could not create BatchIterator in {}",
+            sorter_temp_file.display()
+        )
+    })
 }