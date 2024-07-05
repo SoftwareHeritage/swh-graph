3e73615d512c65264dc2cca7bf85483200165692
Stefano Zacchiroli <zack@upsilon.cc>
2024-07-05 11:14:02 +0200

    review *.rs file headers: add missing Copyright decl
    Also fix a couple of things noticed while doing this:
    
    - Ensure //! are used where relevant
    - Remove an empty utils file exhaustivegroupby.rs

--- rust/src/java_compat/mod.rs
+++ rust/src/java_compat/mod.rs
@@ -1,7 +1,12 @@
-/// Structures to load file created with legacy swh-graph implementation
-/// written in Java.
-///
-/// `mph`, and `sf` are imported [from sux-rs](https://archive.softwareheritage.org/swh:1:dir:5aef0244039204e3cbe1424f645c9eadcc80956f;origin=https://github.com/vigna/sux-rs;visit=swh:1:snp:855180f9102fd3d7451e98f293cdd90cff7f17d9;anchor=swh:1:rev:9cafac06c95c2d916b76dc374a6f9d976bf65456)
+// Copyright (C) 2023  The Software Heritage developers
+// See the AUTHORS file at the top-level directory of this distribution
+// License: GNU General Public License version 3, or any later version
+// See top-level LICENSE file for more information
+
+//! Structures to load file created with legacy swh-graph implementation
+//! written in Java.
+//!
+//! `mph`, and `sf` are imported [from sux-rs](https://archive.softwareheritage.org/swh:1:dir:5aef0244039204e3cbe1424f645c9eadcc80956f;origin=https://github.com/vigna/sux-rs;visit=swh:1:snp:855180f9102fd3d7451e98f293cdd90cff7f17d9;anchor=swh:1:rev:9cafac06c95c2d916b76dc374a6f9d976bf65456)
 pub mod bit_vector;
 pub mod fcl;
 pub mod mph;
