611b6e44c1bc4fa1399f6de956351993b1d7463a
Stefano Zacchiroli <zack@upsilon.cc>
2024-08-05 08:29:46 -0700

    Subgraph: migrate tests to GraphBuilder
    also, make tests more thorough, by also testing has_arc

--- rust/tests/subgraph.rs
+++ rust/tests/subgraph.rs
@@ -6,52 +6,69 @@
 use anyhow::Result;
 use std::rc::Rc;
 use swh_graph::graph::*;
-use swh_graph::java_compat::mph::gov::GOVMPH;
+use swh_graph::graph_builder::GraphBuilder;
 use swh_graph::views::Subgraph;
 use swh_graph::{swhid, NodeConstraint};
 
-const BASENAME: &str = "../swh/graph/example_dataset/compressed/example";
+mod data;
 
 #[test]
 fn test_node_constraint() -> Result<()> {
-    let graph = load_unidirectional(BASENAME)?
-        .load_all_properties::<GOVMPH>()?
-        .load_labels()?;
-    let graph = Rc::new(graph);
+    let mut builder = GraphBuilder::default();
+    let ori01 = builder
+        .node(swhid!(swh:1:ori:0000000000000000000000000000000000000001))?
+        .done();
+    let snp20 = builder
+        .node(swhid!(swh:1:snp:0000000000000000000000000000000000000020))?
+        .done();
+    let rev09 = builder
+        .node(swhid!(swh:1:rev:0000000000000000000000000000000000000009))?
+        .done();
+    let dir08 = builder
+        .node(swhid!(swh:1:dir:0000000000000000000000000000000000000008))?
+        .done();
+    let cnt01 = builder
+        .node(swhid!(swh:1:cnt:0000000000000000000000000000000000000001))?
+        .done();
+    builder.arc(ori01, snp20);
+    builder.arc(snp20, rev09);
+    builder.arc(rev09, dir08);
+    builder.arc(dir08, cnt01);
+    let graph = Rc::new(builder.done()?);
+    let props = graph.properties();
+
+    let ori_node = props.node_id(swhid!(swh:1:ori:0000000000000000000000000000000000000001))?;
+    let snp_node = props.node_id(swhid!(swh:1:snp:0000000000000000000000000000000000000020))?;
+    let rev_node = props.node_id(swhid!(swh:1:rev:0000000000000000000000000000000000000009))?;
+    let dir_node = props.node_id(swhid!(swh:1:dir:0000000000000000000000000000000000000008))?;
+    let cnt_node = props.node_id(swhid!(swh:1:cnt:0000000000000000000000000000000000000001))?;
 
     let full_graph =
         Subgraph::with_node_constraint(graph.clone(), "*".parse::<NodeConstraint>().unwrap());
-    let props = full_graph.properties();
-    // assert_eq!(full_graph.num_nodes(), graph.num_nodes());  // num_nodes() is no-op on subgraphs
-    let cnt_node = props.node_id(swhid!(swh:1:cnt:0000000000000000000000000000000000000001))?;
-    let rel_node = props.node_id(swhid!(swh:1:rel:0000000000000000000000000000000000000010))?;
-    let snp_node = props.node_id(swhid!(swh:1:snp:0000000000000000000000000000000000000020))?;
-    assert!(full_graph.has_node(cnt_node));
-    assert!(full_graph.has_node(rel_node));
     assert!(full_graph.has_node(snp_node));
+    assert!(full_graph.has_node(cnt_node));
+    assert!(full_graph.has_arc(ori_node, snp_node));
+    assert!(full_graph.has_arc(rev_node, dir_node));
+    assert!(full_graph.has_arc(dir_node, cnt_node));
 
     let fs_graph =
         Subgraph::with_node_constraint(graph.clone(), "dir,cnt".parse::<NodeConstraint>().unwrap());
-    let props = fs_graph.properties();
-    let cnt_node = props.node_id(swhid!(swh:1:cnt:0000000000000000000000000000000000000001))?;
-    let dir_node = props.node_id(swhid!(swh:1:dir:0000000000000000000000000000000000000002))?;
-    // assert_eq!(fs_graph.num_nodes(), 13);  // num_nodes() is no-op on subgraphs
     assert!(fs_graph.has_node(cnt_node));
     assert!(fs_graph.has_node(dir_node));
+    assert!(!fs_graph.has_node(rev_node));
     assert!(fs_graph.has_arc(dir_node, cnt_node));
-    assert!(!fs_graph
-        .has_node(props.node_id(swhid!(swh:1:rev:0000000000000000000000000000000000000003))?));
+    assert!(!fs_graph.has_arc(ori_node, snp_node));
+    assert!(!fs_graph.has_arc(rev_node, dir_node));
 
-    let history_graph = Subgraph::with_node_constraint(graph.clone(), "rel,rev".parse().unwrap());
-    let props = history_graph.properties();
-    let rel_node = props.node_id(swhid!(swh:1:rel:0000000000000000000000000000000000000010))?;
-    let rev_node = props.node_id(swhid!(swh:1:rev:0000000000000000000000000000000000000009))?;
-    // assert_eq!(history_graph.num_nodes(), 7);  // num_nodes() is no-op on subgraphs
-    assert!(history_graph.has_node(rel_node));
+    let history_graph =
+        Subgraph::with_node_constraint(graph.clone(), "snp,rel,rev".parse().unwrap());
+    assert!(history_graph.has_node(snp_node));
     assert!(history_graph.has_node(rev_node));
-    assert!(history_graph.has_arc(rel_node, rev_node));
-    assert!(!history_graph
-        .has_node(props.node_id(swhid!(swh:1:cnt:0000000000000000000000000000000000000001))?));
+    assert!(!history_graph.has_node(ori_node));
+    assert!(!history_graph.has_node(cnt_node));
+    assert!(!history_graph.has_node(dir_node));
+    assert!(history_graph.has_arc(snp_node, rev_node));
+    assert!(!history_graph.has_arc(rev_node, dir_node));
 
     Ok(())
 }
