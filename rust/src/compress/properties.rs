62ed3bf9224fa8ba72ae790f5c5de82ca5ed2c7b
Valentin Lorentz <vlorentz@softwareheritage.org>
2024-08-07 17:22:12 +0200

    properties: Update arrays atomically and remove LongArrayBitVector
    It is unsound to update an array from multiple threads using non-atomic
    operations, even though two threads can't update the same cell.
    
    Additionally, this changes the format of is_skipped.bits to be an actual
    bitvec instead of LongArrayBitVector (which would be is_skipped.bin).

--- rust/src/compress/properties.rs
+++ rust/src/compress/properties.rs
@@ -5,15 +5,16 @@
 
 use std::io::{BufWriter, Write};
 use std::path::PathBuf;
+use std::sync::atomic::Ordering;
 
 use anyhow::{Context, Result};
 use ar_row::deserialize::{ArRowDeserialize, ArRowStruct};
 use ar_row_derive::ArRowDeserialize;
+use common_traits::{Atomic, IntoAtomic};
 use rayon::prelude::*;
 
 use super::orc::get_dataset_readers;
 use super::orc::{iter_arrow, par_iter_arrow};
-use crate::java_compat::bit_vector::LongArrayBitVector;
 use crate::map::{MappedPermutation, Permutation};
 use crate::mph::SwhidMphf;
 use crate::properties::suffixes;
@@ -69,6 +70,19 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         vec.par_iter_mut().for_each(|v| *v = initial_value);
         vec
     }
+    /// Same as [`Self::init_vec`] but returns a vector of atomic values
+    fn init_atomic_vec<T: IntoAtomic + Copy + Default + Sync>(
+        &self,
+        initial_value: T,
+    ) -> Vec<<T as IntoAtomic>::AtomicType>
+    where
+        for<'a> Vec<T>: IntoParallelRefMutIterator<'a, Item = &'a mut T>,
+    {
+        (0..self.num_nodes)
+            .into_par_iter()
+            .map(|_| initial_value.to_atomic())
+            .collect()
+    }
 
     fn node_id(&self, swhid: &str) -> usize {
         self.order
@@ -80,14 +94,22 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
             .unwrap()
     }
 
-    fn set<Value>(&self, vector: &[Value], swhid: &str, value: Value) {
-        unsafe {
-            vector
-                .as_ptr()
-                .add(self.node_id(swhid))
-                .cast_mut()
-                .write(value)
-        };
+    fn set_atomic<Value: Atomic>(
+        &self,
+        vector: &[Value],
+        swhid: &str,
+        value: Value::NonAtomicType,
+    ) {
+        vector
+            .get(self.node_id(swhid))
+            .expect("node_id is larger than the array")
+            .store(value, Ordering::Relaxed)
+    }
+
+    fn set<Value>(&self, vector: &mut [Value], swhid: &str, value: Value) {
+        *vector
+            .get_mut(self.node_id(swhid))
+            .expect("node_id is larger than the array") = value;
     }
 
     fn write<Value: bytemuck::Pod>(&self, suffix: &str, values: impl AsRef<[Value]>) -> Result<()> {
@@ -98,6 +120,15 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
             .with_context(|| format!("Could not write to {}", path.display()))?;
 
         Ok(())
+    }
+
+    fn write_atomic<Value: Atomic>(&self, suffix: &str, values: Vec<Value>) -> Result<()>
+    where
+        <Value as Atomic>::NonAtomicType: bytemuck::Pod,
+    {
+        // In release mode, this is compiled into a no-op
+        let values: Vec<_> = values.into_iter().map(Value::into_inner).collect();
+        self.write(suffix, values)
     }
 
     pub fn write_author_timestamps(&self) -> Result<()> {
@@ -117,14 +148,14 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         }
 
         log::info!("Initializing...");
-        let timestamps = self.init_vec(i64::MIN.to_be());
-        let timestamp_offsets = self.init_vec(i16::MIN.to_be());
+        let timestamps = self.init_atomic_vec(i64::MIN.to_be());
+        let timestamp_offsets = self.init_atomic_vec(i16::MIN.to_be());
 
         log::info!("Reading...");
         let f = |type_: &str, r: Revrel| {
             if let Some(date) = r.date {
                 let swhid = format!("swh:1:{}:{}", type_, r.id);
-                self.set(
+                self.set_atomic(
                     &timestamps,
                     &swhid,
                     i64::try_from(date.0.div_euclid(NANOSECONDS_IN_SECOND))
@@ -132,7 +163,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
                         .to_be(),
                 );
                 if let Some(date_offset) = r.date_offset {
-                    self.set(&timestamp_offsets, &swhid, date_offset.to_be());
+                    self.set_atomic(&timestamp_offsets, &swhid, date_offset.to_be());
                 }
             }
         };
@@ -153,8 +184,8 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         }
 
         log::info!("Writing...");
-        self.write(suffixes::AUTHOR_TIMESTAMP, timestamps)?;
-        self.write(suffixes::AUTHOR_TIMESTAMP_OFFSET, timestamp_offsets)?;
+        self.write_atomic(suffixes::AUTHOR_TIMESTAMP, timestamps)?;
+        self.write_atomic(suffixes::AUTHOR_TIMESTAMP_OFFSET, timestamp_offsets)?;
 
         Ok(())
     }
@@ -172,14 +203,14 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         }
 
         log::info!("Initializing...");
-        let timestamps = self.init_vec(i64::MIN.to_be());
-        let timestamp_offsets = self.init_vec(i16::MIN.to_be());
+        let timestamps = self.init_atomic_vec(i64::MIN.to_be());
+        let timestamp_offsets = self.init_atomic_vec(i16::MIN.to_be());
 
         log::info!("Reading...");
         self.par_for_each_row("revision", |rev: Revision| {
             if let Some(date) = rev.committer_date {
                 let swhid = format!("swh:1:rev:{}", rev.id);
-                self.set(
+                self.set_atomic(
                     &timestamps,
                     &swhid,
                     i64::try_from(date.0.div_euclid(NANOSECONDS_IN_SECOND))
@@ -187,15 +218,15 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
                         .to_be(),
                 );
                 if let Some(date_offset) = rev.committer_offset {
-                    self.set(&timestamp_offsets, &swhid, date_offset.to_be());
+                    self.set_atomic(&timestamp_offsets, &swhid, date_offset.to_be());
                 }
             }
         })?
         .for_each(|()| ());
 
         log::info!("Writing...");
-        self.write(suffixes::COMMITTER_TIMESTAMP, timestamps)?;
-        self.write(suffixes::COMMITTER_TIMESTAMP_OFFSET, timestamp_offsets)?;
+        self.write_atomic(suffixes::COMMITTER_TIMESTAMP, timestamps)?;
+        self.write_atomic(suffixes::COMMITTER_TIMESTAMP_OFFSET, timestamp_offsets)?;
 
         Ok(())
     }
@@ -212,14 +243,14 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         }
 
         log::info!("Initializing...");
-        let lengths = self.init_vec(u64::MAX.to_be());
+        let lengths = self.init_atomic_vec(u64::MAX.to_be());
 
         log::info!("Reading...");
         let f = |cnt: Content| {
             if let Some(id) = cnt.sha1_git {
                 if let Some(length) = cnt.length {
                     let swhid = format!("swh:1:cnt:{}", id);
-                    self.set(&lengths, &swhid, (length as u64).to_be());
+                    self.set_atomic(&lengths, &swhid, (length as u64).to_be());
                 }
             }
         };
@@ -229,7 +260,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
             .for_each(|()| ());
 
         log::info!("Writing...");
-        self.write(suffixes::CONTENT_LENGTH, lengths)?;
+        self.write_atomic(suffixes::CONTENT_LENGTH, lengths)?;
 
         Ok(())
     }
@@ -260,18 +291,17 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         })?
         .for_each(|()| ());
 
-        // We need to be compatible with graphs generated with Java, which used Java's
-        // native serialization for it.unimi.dsi.bits.LongArrayBitVector, which
-        // is little-endian on disk; but sux::bits::bit_vec::BitVec is big endian
-        // in memory.
         log::info!("Converting...");
-        let is_skipped: sux::bits::bit_vec::BitVec<Vec<usize>> = is_skipped.into();
-        let bitvec = LongArrayBitVector::new_from_bitvec(is_skipped);
+        let (bitvec, len) = is_skipped.into_raw_parts();
+        assert_eq!(len, self.num_nodes);
+        // Make its values big-endian
+        let bitvec_be: Vec<u8> = bitvec
+            .into_par_iter()
+            .flat_map(|cell| cell.into_inner().to_be_bytes())
+            .collect();
 
         log::info!("Writing...");
-        bitvec
-            .dump(suffix_path(&self.target, suffixes::CONTENT_IS_SKIPPED))
-            .context("Could not write LongArrayBitVector")?;
+        self.write(suffixes::CONTENT_IS_SKIPPED, &bitvec_be)?;
 
         Ok(())
     }
@@ -298,7 +328,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         };
 
         log::info!("Initializing...");
-        let authors = self.init_vec(u32::MAX.to_be());
+        let authors = self.init_atomic_vec(u32::MAX.to_be());
 
         log::info!("Reading...");
         let f = |type_: &str, r: Revrel| {
@@ -310,7 +340,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
                     .get_byte_array(&person)
                     .try_into()
                     .expect("Person id overflows u32");
-                self.set(&authors, &swhid, person_id.to_be());
+                self.set_atomic(&authors, &swhid, person_id.to_be());
             }
         };
 
@@ -330,7 +360,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         }
 
         log::info!("Writing...");
-        self.write(suffixes::AUTHOR_ID, authors)?;
+        self.write_atomic(suffixes::AUTHOR_ID, authors)?;
         Ok(())
     }
     pub fn write_committer_ids(&self) -> Result<()> {
@@ -353,7 +383,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         };
 
         log::info!("Initializing...");
-        let committers = self.init_vec(u32::MAX.to_be());
+        let committers = self.init_atomic_vec(u32::MAX.to_be());
 
         log::info!("Reading...");
         self.par_for_each_row("revision", |rev: Revision| {
@@ -365,13 +395,13 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
                     .get_byte_array(&person)
                     .try_into()
                     .expect("Person id overflows u32");
-                self.set(&committers, &swhid, person_id.to_be());
+                self.set_atomic(&committers, &swhid, person_id.to_be());
             }
         })?
         .for_each(|()| ());
 
         log::info!("Writing...");
-        self.write(suffixes::COMMITTER_ID, committers)?;
+        self.write_atomic(suffixes::COMMITTER_ID, committers)?;
         Ok(())
     }
     pub fn write_messages(&self) -> Result<()> {
@@ -396,7 +426,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         }
 
         log::info!("Initializing...");
-        let offsets = self.init_vec(u64::MAX.to_be());
+        let mut offsets = self.init_vec(u64::MAX.to_be());
         let path = suffix_path(&self.target, suffixes::MESSAGE);
         let file = std::fs::File::create(&path)
             .with_context(|| format!("Could not create {}", path.display()))?;
@@ -412,7 +442,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
                 encoded_message.push('\n');
                 let encoded_message = encoded_message.as_bytes();
                 writer.write_all(encoded_message)?;
-                self.set(&offsets, &swhid, offset.to_be());
+                self.set(&mut offsets, &swhid, offset.to_be());
                 offset += encoded_message.len() as u64;
             }
             Ok(())
@@ -451,7 +481,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
         }
 
         log::info!("Initializing...");
-        let offsets = self.init_vec(u64::MAX.to_be());
+        let mut offsets = self.init_vec(u64::MAX.to_be());
         let path = suffix_path(&self.target, suffixes::TAG_NAME);
         let file = std::fs::File::create(&path)
             .with_context(|| format!("Could not create {}", path.display()))?;
@@ -468,7 +498,7 @@ impl<SWHIDMPHF: SwhidMphf + Sync> PropertyWriter<SWHIDMPHF> {
             encoded_name.push('\n');
             let encoded_name = encoded_name.as_bytes();
             writer.write_all(encoded_name)?;
-            self.set(&offsets, &swhid, offset.to_be());
+            self.set(&mut offsets, &swhid, offset.to_be());
             offset += encoded_name.len() as u64;
 
             Ok(())
