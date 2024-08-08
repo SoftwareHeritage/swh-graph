d939d1233e9f2752eeec125e7dfb51837359592c
Valentin Lorentz <vlorentz@softwareheritage.org>
2024-08-08 15:00:44 +0200

    Add 'compare-graphs' tool
    To check two graphs generated by different implementations of the compression
    pipeline are identical

---
+++ rust/examples/compare-graphs.rs
@@ -0,0 +1,355 @@
+/*
+ * Copyright (C) 2023-2024  The Software Heritage developers
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ */
+
+use std::collections::HashSet;
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+
+use anyhow::{bail, ensure, Context, Result};
+use clap::Parser;
+use dsi_progress_logger::ProgressLogger;
+use rayon::prelude::*;
+
+use swh_graph::graph::*;
+use swh_graph::java_compat::mph::gov::GOVMPH;
+use swh_graph::utils::shuffle::par_iter_shuffled_range;
+use swh_graph::views::Transposed;
+use swh_graph::NodeType;
+
+#[derive(Parser, Debug)]
+/// Reads two graphs and check they have the same labels and properties
+struct Args {
+    graph1: PathBuf,
+    graph2: PathBuf,
+    #[arg(long)]
+    /// Assume without checking SWHIDs are the same in both graphs
+    skip_swhids: bool,
+    #[arg(long)]
+    /// Assume without checking successors are the same in both graphs
+    skip_successors: bool,
+    #[arg(long)]
+    /// Do not check arc labels are the same in both graphs
+    skip_arc_labels: bool,
+    #[arg(long)]
+    /// Do not check node properties are the same in both graphs
+    skip_properties: bool,
+    #[arg(long)]
+    /// Needed when comparing graphs with origin->snapshot labels *and* generated with Java
+    /// to compensate for the Java implementation dropping all but one labels when there are
+    /// multiple visits of the same origin with the same timestamp at the same timestamp.
+    allow_missing_duplicate_origin_arc_labels: bool,
+    #[arg(long)]
+    /// Check backwards graphs instead of forwards graphs
+    transposed: bool,
+}
+
+pub fn main() -> Result<()> {
+    let args = Args::parse();
+
+    stderrlog::new()
+        .verbosity(2)
+        .timestamp(stderrlog::Timestamp::Second)
+        .init()
+        .with_context(|| "While Initializing the stderrlog")?;
+
+    let graph1 = load_bidirectional(&args.graph1)
+        .with_context(|| format!("Could not load graph {}", args.graph1.display()))?
+        .load_all_properties::<GOVMPH>()
+        .with_context(|| {
+            format!(
+                "Could not load properties from graph {}",
+                args.graph1.display()
+            )
+        })?
+        .load_labels()
+        .with_context(|| format!("Could not load labels from graph {}", args.graph1.display()))?;
+
+    let graph2 = load_bidirectional(&args.graph2)
+        .with_context(|| format!("Could not load graph {}", args.graph2.display()))?
+        .load_all_properties::<GOVMPH>()
+        .with_context(|| {
+            format!(
+                "Could not load properties from graph {}",
+                args.graph2.display()
+            )
+        })?
+        .load_labels()
+        .with_context(|| format!("Could not load labels from graph {}", args.graph2.display()))?;
+
+    ensure!(
+        graph1.num_nodes() == graph2.num_nodes(),
+        "{} has {} nodes, {} has {} nodes",
+        args.graph1.display(),
+        graph1.num_nodes(),
+        args.graph2.display(),
+        graph2.num_nodes()
+    );
+
+    if args.transposed {
+        let graph1 = Transposed(graph1);
+        let graph2 = Transposed(graph2);
+
+        if !args.skip_swhids {
+            check_swhids(&graph1, &graph2)?;
+        }
+
+        if !args.skip_successors {
+            check_successors(&graph1, &graph2)?;
+        }
+
+        if !args.skip_arc_labels {
+            check_labels(
+                &graph1,
+                &graph2,
+                args.allow_missing_duplicate_origin_arc_labels,
+            )?;
+        }
+
+        if !args.skip_properties {
+            check_properties(&graph1, &graph2)?;
+        }
+    } else {
+        if !args.skip_swhids {
+            check_swhids(&graph1, &graph2)?;
+        }
+
+        if !args.skip_successors {
+            check_successors(&graph1, &graph2)?;
+        }
+
+        if !args.skip_arc_labels {
+            check_labels(
+                &graph1,
+                &graph2,
+                args.allow_missing_duplicate_origin_arc_labels,
+            )?;
+        }
+
+        if !args.skip_properties {
+            check_properties(&graph1, &graph2)?;
+        }
+    }
+
+    // TODO: check label names
+
+    Ok(())
+}
+
+fn check_swhids<G>(graph1: &G, graph2: &G) -> Result<()>
+where
+    G: SwhGraph + SwhGraphWithProperties + Sync,
+    <G as SwhGraphWithProperties>::Maps: swh_graph::properties::Maps,
+{
+    let num_nodes = graph2.num_nodes();
+    let mut pl = ProgressLogger::default().display_memory();
+    pl.item_name = "node";
+    pl.expected_updates = Some(num_nodes);
+    pl.start("Checking SWHIDs...");
+    let pl = Arc::new(Mutex::new(pl));
+    (0..num_nodes)
+        .into_par_iter()
+        .try_for_each(|node| -> Result<()> {
+            if node % 32768 == 0 {
+                pl.lock().unwrap().update_with_count(32768);
+            }
+
+            if graph1.properties().swhid(node) != graph1.properties().swhid(node) {
+                log::error!(
+                    "node {} is {} in {} and {} in {}",
+                    node,
+                    graph1.properties().swhid(node),
+                    graph1.path().display(),
+                    graph2.properties().swhid(node),
+                    graph2.path().display()
+                );
+                bail!("SWHID mismatch")
+            }
+
+            Ok(())
+        })?;
+    pl.lock().unwrap().done();
+
+    Ok(())
+}
+
+fn check_successors<G>(graph1: &G, graph2: &G) -> Result<()>
+where
+    G: SwhGraph + SwhForwardGraph + SwhGraphWithProperties + Sync,
+    <G as SwhGraphWithProperties>::Maps: swh_graph::properties::Maps,
+{
+    let num_nodes = graph2.num_nodes();
+    let mut pl = ProgressLogger::default().display_memory();
+    pl.item_name = "node";
+    pl.expected_updates = Some(num_nodes);
+    pl.start("Checking successors...");
+    let pl = Arc::new(Mutex::new(pl));
+    par_iter_shuffled_range(0..num_nodes).try_for_each(|node| -> Result<()> {
+        let successors1: Vec<_> = graph1.successors(node).into_iter().collect();
+        let successors2: Vec<_> = graph2.successors(node).into_iter().collect();
+        if successors1 != successors2 {
+            log::error!(
+                "node {} has successors {} in {} and {} in {}",
+                node,
+                successors1
+                    .into_iter()
+                    .map(|succ| graph1.properties().swhid(succ).to_string())
+                    .collect::<Vec<_>>()
+                    .join(", "),
+                graph1.path().display(),
+                successors2
+                    .into_iter()
+                    .map(|succ| graph2.properties().swhid(succ).to_string())
+                    .collect::<Vec<_>>()
+                    .join(", "),
+                graph2.path().display()
+            );
+            bail!("Successors mismatch")
+        }
+
+        if node % 32768 == 0 {
+            pl.lock().unwrap().update_with_count(32768);
+        }
+
+        Ok(())
+    })?;
+    pl.lock().unwrap().done();
+
+    Ok(())
+}
+
+fn check_labels<G>(
+    graph1: &G,
+    graph2: &G,
+    allow_missing_duplicate_origin_arc_labels: bool,
+) -> Result<()>
+where
+    G: SwhGraph + SwhLabeledForwardGraph + SwhGraphWithProperties + Sync,
+    <G as SwhGraphWithProperties>::Maps: swh_graph::properties::Maps,
+{
+    assert_eq!(graph1.is_transposed(), graph2.is_transposed());
+    let num_nodes = graph2.num_nodes();
+    let mut pl = ProgressLogger::default().display_memory();
+    pl.item_name = "node";
+    pl.expected_updates = Some(num_nodes);
+    pl.start("Checking arc labels...");
+    let pl = Arc::new(Mutex::new(pl));
+    par_iter_shuffled_range(0..num_nodes).try_for_each(|node| -> Result<()> {
+        let mut num_arcs = 0;
+        let mut num_labels = 0;
+        for ((succ1, labels1), (succ2, labels2)) in graph1
+            .labeled_successors(node)
+            .zip(graph2.labeled_successors(node))
+        {
+            ensure!(succ1 == succ2, "Successors mismatch");
+            let labels1: Vec<_> = labels1.collect();
+            let labels2: Vec<_> = labels2.collect();
+            if labels1 != labels2 {
+                let is_ori_to_snp_arc = if graph1.is_transposed() {
+                    graph1.properties().node_type(succ1) == NodeType::Origin
+                } else {
+                    graph1.properties().node_type(node) == NodeType::Origin
+                };
+                if allow_missing_duplicate_origin_arc_labels && is_ori_to_snp_arc && labels1.iter().copied().collect::<HashSet<_>>() == labels2.iter().copied().collect::<HashSet<_>>() {
+                    log::warn!(
+                        "arc {} ({}) -> {} ({}) has same set of labels, but different counts ({} in {} and {} in {})",
+                        graph1.properties().swhid(node),
+                        node,
+                        graph1.properties().swhid(succ1),
+                        succ1,
+                        labels1.len(),
+                        graph1.path().display(),
+                        labels2.len(),
+                        graph2.path().display()
+                    );
+                } else {
+                    log::error!(
+                        "arc {} ({}) -> {} ({}) has labels {:?} in {} and {:?} in {}",
+                        graph1.properties().swhid(node),
+                        node,
+                        graph1.properties().swhid(succ1),
+                        succ1,
+                        labels1,
+                        graph1.path().display(),
+                        labels2,
+                        graph2.path().display(),
+                    );
+                    bail!("Labels mismatch")
+                }
+            }
+            num_arcs += 1;
+            num_labels += labels1.len();
+        }
+
+        //log::info!("{} ({}) ok ({} arcs, {} labels)", graph1.properties().swhid(node), node, num_arcs, num_labels);
+
+        if node % 32768 == 0 {
+            pl.lock().unwrap().update_with_count(32768);
+        }
+        Ok(())
+    })?;
+    pl.lock().unwrap().done();
+    Ok(())
+}
+
+fn check_properties<G>(graph1: &G, graph2: &G) -> Result<()>
+where
+    G: SwhGraph + SwhForwardGraph + SwhGraphWithProperties + Sync,
+    <G as SwhGraphWithProperties>::Contents: swh_graph::properties::Contents,
+    <G as SwhGraphWithProperties>::Maps: swh_graph::properties::Maps,
+    <G as SwhGraphWithProperties>::Persons: swh_graph::properties::Persons,
+    <G as SwhGraphWithProperties>::Strings: swh_graph::properties::Strings,
+    <G as SwhGraphWithProperties>::Timestamps: swh_graph::properties::Timestamps,
+{
+    let num_nodes = graph1.num_nodes();
+
+    macro_rules! check_property {
+        ($property:ident) => {{
+            let mut pl = ProgressLogger::default().display_memory();
+            pl.item_name = "node";
+            pl.expected_updates = Some(num_nodes);
+            pl.start(&format!("Checking {}...", stringify!($property)));
+            let pl = Arc::new(Mutex::new(pl));
+            (0..num_nodes)
+                .into_par_iter()
+                .try_for_each(|node| -> Result<()> {
+                    if graph1.properties().$property(node) != graph2.properties().$property(node) {
+                        log::error!(
+                            "node {} ({}) has {} {:?} in {} and {:?} in {}",
+                            node,
+                            graph1.properties().swhid(node),
+                            stringify!($property),
+                            graph1.properties().$property(node),
+                            graph1.path().display(),
+                            graph2.properties().$property(node),
+                            graph2.path().display()
+                        );
+                    }
+
+                    if node % 32768 == 0 {
+                        pl.lock().unwrap().update_with_count(32768);
+                    }
+
+                    Ok(())
+                })?;
+
+            pl.lock().unwrap().done();
+        }};
+    }
+
+    check_property!(author_timestamp);
+    check_property!(author_timestamp_offset);
+    check_property!(committer_timestamp);
+    check_property!(committer_timestamp_offset);
+    check_property!(author_id);
+    check_property!(committer_id);
+    check_property!(content_length);
+    check_property!(is_skipped_content);
+    check_property!(message);
+    check_property!(tag_name);
+
+    Ok(())
+}
