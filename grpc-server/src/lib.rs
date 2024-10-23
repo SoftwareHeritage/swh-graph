2b008704d8816f3f1292d88de9816545bd1436f5
Valentin Lorentz <vlorentz@softwareheritage.org>
2024-10-23 17:41:39 +0200

    Make Stats::avg_locality optional
    It is absent from graph.stats files computed by the Rust pipeline

--- grpc-server/src/lib.rs
+++ grpc-server/src/lib.rs
@@ -221,7 +221,7 @@ impl<G: SwhFullGraph + Send + Sync + Clone + 'static>
             compression_ratio: get_property(&properties, properties_path, "compratio").ok(),
             bits_per_node: get_property(&properties, properties_path, "bitspernode").ok(),
             bits_per_edge: get_property(&properties, properties_path, "bitsperlink").ok(),
-            avg_locality: get_property(&stats, stats_path, "avglocality")?,
+            avg_locality: get_property(&stats, stats_path, "avglocality").ok(),
             indegree_min: get_property(&stats, stats_path, "minindegree")?,
             indegree_max: get_property(&stats, stats_path, "maxindegree")?,
             indegree_avg: get_property(&stats, stats_path, "avgindegree")?,
