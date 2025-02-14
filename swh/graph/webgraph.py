5cf8107f32806348f927a97ee5a729f8ccf526ec
Antoine Lambert <anlambert@softwareheritage.org>
2025-02-14 11:13:49 +0100

    webgraph: Fix black formatting

--- swh/graph/webgraph.py
+++ swh/graph/webgraph.py
@@ -3,9 +3,7 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-"""WebGraph driver
-
-"""
+"""WebGraph driver"""
 
 from datetime import datetime
 import difflib
