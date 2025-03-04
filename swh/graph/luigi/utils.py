3153b2009592558c56d0e08078428612c42ce2eb
Valentin Lorentz <vlorentz@softwareheritage.org>
2025-03-04 12:02:09 +0100

    Expose a base Luigi Task class to upload to S3 without creating an Athena table
    This will be used by swh-provenance, as its tables cannot be queried
    efficiently by Athena, and it's faster to upload all tables in parallel
    (which we is harder if each table has to have its own Task class).

--- swh/graph/luigi/utils.py
+++ swh/graph/luigi/utils.py
@@ -52,9 +52,11 @@ def count_nodes(local_graph_path: Path, graph_name: str, object_types: str) -> i
     return sum(int(nb_nodes_per_type[type_]) for type_ in object_types.split(","))
 
 
-class _ParquetToS3ToAthenaTask(luigi.Task):
-    """Base class for tasks which take a CSV as input, convert it to ORC,
-    upload the ORC to S3, and create an Athena table for it."""
+class _ParquetToS3Task(luigi.Task):
+    """Base class for tasks which take a local Parquet table as input, upload it to S3.
+
+    See :class:`_ParquetToS3ToAthenaTask` for tasks that also need to create an
+    Athena table."""
 
     parallelism = 10
 
@@ -67,12 +69,12 @@ class _ParquetToS3ToAthenaTask(luigi.Task):
     def _s3_prefix(self) -> str:
         raise NotImplementedError(f"{self.__class__.__name__}._s3_prefix")
 
-    def _parquet_columns(self) -> List[Tuple[str, str]]:
-        """Returns a list of ``(column_name, parquet_type)``"""
-        raise NotImplementedError(f"{self.__class__.__name__}._parquet_columns")
+    def _s3_path(self) -> str:
+        """Concatenates ``s3://``, :meth:`_s3_bucket`, and :meth:`_s3_prefix`."""
+        return f"s3://{self._s3_bucket()}/{self._s3_prefix()}"
 
     def _approx_nb_rows(self) -> int:
-        """Returns number of rows in the CSV file. Used only for progress reporting"""
+        """Returns number of rows in the Parquet file. Used only for progress reporting"""
         import pyarrow.parquet
 
         return sum(
@@ -80,17 +82,14 @@ class _ParquetToS3ToAthenaTask(luigi.Task):
             for file in self._input_parquet_path().iterdir()
         )
 
-    def _athena_db_name(self) -> str:
-        raise NotImplementedError(f"{self.__class__.__name__}._athena_db_name")
-
-    def _athena_table_name(self) -> str:
-        raise NotImplementedError(f"{self.__class__.__name__}._athena_table_name")
-
     def output(self) -> luigi.Target:
-        return AthenaDatabaseTarget(self._athena_db_name(), {self._athena_table_name()})
+        import luigi.contrib.s3
 
-    def run(self) -> None:
-        """Copies all files: first the graph itself, then :file:`meta/compression.json`."""
+        return luigi.contrib.s3.S3FlagTarget(self._s3_path())
+
+    def _upload_files(self):
+        """Copies all files in parallel from the path returned by :meth:`_input_parquet_path`
+        to the S3 bucket and path returned by :meth:`_s3_path`."""
         import multiprocessing
 
         import tqdm
@@ -103,13 +102,10 @@ class _ParquetToS3ToAthenaTask(luigi.Task):
             for i, relative_path in tqdm.tqdm(
                 enumerate(p.imap_unordered(self._upload_file, paths)),
                 total=len(paths),
-                desc=f"Uploading {self._input_parquet_path()} to "
-                f"s3://{self._s3_bucket()}/{self._s3_prefix()}/",
+                desc=f"Uploading {self._input_parquet_path()} to {self._s3_path()}",
             ):
                 self.set_progress_percentage(int(i * 100 / len(paths)))
                 self.set_status_message("\n".join(self.__status_messages.values()))
-
-        self._create_athena_table()
 
     def _upload_file(self, path):
         import luigi.contrib.s3
@@ -122,13 +118,41 @@ class _ParquetToS3ToAthenaTask(luigi.Task):
 
         client.put_multipart(
             path,
-            f"s3://{self._s3_bucket()}/{self._s3_prefix()}/{relative_path}",
+            f"{self._s3_path()}/{relative_path}",
             ACL="public-read",
         )
 
         del self.__status_messages[path]
 
         return relative_path
+
+    def run(self) -> None:
+        """Calls :meth:`_upload_files`, then writes a :file:`_SUCCESS` stamp."""
+        self._upload_files()
+
+        client = luigi.contrib.s3.S3Client()
+        client.put_string("success", f"{self._s3_path()}/_SUCCESS")
+
+
+class _ParquetToS3ToAthenaTask(_ParquetToS3Task):
+    def _parquet_columns(self) -> List[Tuple[str, str]]:
+        """Returns a list of ``(column_name, parquet_type)``"""
+        raise NotImplementedError(f"{self.__class__.__name__}._parquet_columns")
+
+    def _athena_db_name(self) -> str:
+        raise NotImplementedError(f"{self.__class__.__name__}._athena_db_name")
+
+    def _athena_table_name(self) -> str:
+        raise NotImplementedError(f"{self.__class__.__name__}._athena_table_name")
+
+    def output(self) -> luigi.Target:
+        return AthenaDatabaseTarget(self._athena_db_name(), {self._athena_table_name()})
+
+    def run(self) -> None:
+        """Calls :meth:`_upload_files` then :meth:`_create_athena_table`"""
+        self._upload_files()
+
+        self._create_athena_table()
 
     def _create_athena_table(self):
         import boto3
