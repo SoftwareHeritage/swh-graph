e051daeb666d6f8c18f014b4d701434ef62a4cd7
Valentin Lorentz <vlorentz@softwareheritage.org>
2026-02-07 17:41:55 +0100

    Update copyright years

--- rust/src/views/webgraph.rs
+++ rust/src/views/webgraph.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2025  The Software Heritage developers
+// Copyright (C) 2025-2026  The Software Heritage developers
 // Copyright (C) 2025  Tommaso Fontana
 // See the AUTHORS file at the top-level directory of this distribution
 // License: GNU General Public License version 3, or any later version
